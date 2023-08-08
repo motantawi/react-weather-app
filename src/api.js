c26a989068bcce69bf6350016844d64e13fd8a9c
Mustafa Tantawy <106565969+motantawi@users.noreply.github.com>
2023-08-08 13:33:15 +0300

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "599290907bmsh8183fbbcbea82e1p173fbejsn756d28dbfe78", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0b3f0a074df2c7e97f0ecafbffe09840"; // enter your key from openweather API
